37f3150b9f01221ab54503106fcc84eb36174056
katttalin <kattalinbarcena@gmail.com>
2024-05-20 20:42:49 +0200

    Prueba

--- componentes/NavBar.vue
+++ componentes/NavBar.vue
@@ -1,9 +1,11 @@
 <template>
     <nav>
-        <ul>
+        <div class="LogoContainer">
+            <nuxt-icon name="Logo" filled></nuxt-icon>
+        </div>
+        <ul class="headerMenu">
             <li>
                 <NuxtLink to="/">Tienda</NuxtLink>
-                <IconButton/>
             </li>
             <li>
                 <NuxtLink to="/comunidad">Comunidad</NuxtLink>
@@ -12,9 +14,33 @@
                 <NuxtLink to="/soporte">Soporte</NuxtLink>
             </li>
         </ul>
+        <div class="headerRight">
+            <IconButton/>
+        </div>
     </nav>
 </template>
 
 <script>
-import IconButton from "../componentes/IconButton.vue";
+import IconButton from "./Atoms/IconButton.vue";
 </script>
+
+<style lang="postcss">
+  .nav{
+    display: flex;
+    flex-direction: row;
+  }
+  .LogoContainer{
+    width: auto;
+    .nuxt-icon svg{
+       margin-bottom: 0;
+       width: 20vw;
+       .li{
+        list-style: none;
+       }
+    }
+  }
+  .headerMenu{
+    display: flex;
+    flex-direction: row;
+  }
+</style>
