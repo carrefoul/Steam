67163064d0ad36325cb009f6c37074d262ec82a1
carrefoul <mrotaru5206@esdmadrid.es>
2024-05-21 20:04:59 +0200

    HeaderButton

--- componentes/NavBar.vue
+++ componentes/NavBar.vue
@@ -1,40 +1,79 @@
 <template>
-    <nav class="nav">
-        <div class="LogoContainer">
-            <nuxt-icon name="Logo" filled></nuxt-icon>
-        </div>
-        <ul class="nav-list">
-            <li>
-                <NuxtLink to="/">Tienda</NuxtLink>
-            </li>
-            <li>
-                <NuxtLink to="/comunidad">Comunidad</NuxtLink>
-            </li>
-            <li>
-                <NuxtLink to="/soporte">Soporte</NuxtLink>
-            </li>
-        </ul>
-        <div class="headerRight">
-            <IconButton/>
-        </div>
-    </nav>
+  <nav class="nav">
+    <div class="headerLeft">
+      <div class="LogoContainer">
+        <nuxt-icon name="Logo" class="logo" filled></nuxt-icon>
+      </div>
+      <ul class="nav-list">
+        <li>
+          <HeaderButton to="/" isTitle>Tienda</HeaderButton>
+        </li>
+        <li>
+          <HeaderButton to="/comunidad" isTitle>Comunidad</HeaderButton>
+        </li>
+        <li>
+          <HeaderButton to="/soporte" isTitle>Soporte</HeaderButton>
+        </li>
+      </ul>
+    </div>
+    <div class="headerRight">
+      <IconButton/>
+    </div>
+  </nav>
 </template>
 
 <script>
+import HeaderButton from './Atoms/HeaderButton.vue';
 import IconButton from "./Atoms/IconButton.vue";
+
+export default {
+  components: {
+    HeaderButton,
+    IconButton
+  }
+};
 </script>
 
 <style lang="postcss">
-  .nav{
-    display: flex;
-    justify-content: space-between;
-    .LogoContainer{
-      background-color: white;
-    }
-    .nav-list{
-      list-style: none;
-      display: flex;
-    }
-  }
+.nav {
+  display: flex;
+  justify-content: space-between;
+}
 
+.headerLeft {
+  display: flex;
+  align-items: center;
+}
+
+.LogoContainer {
+  background-color: white;
+  display: flex;
+  align-items: center;
+  height: 100%;
+  overflow: hidden;
+    svg{
+      object-fit: cover;
+      height: 100%;
+    }
+}
+
+.nav-list {
+  list-style: none;
+  display: flex;
+  gap: 10px;
+}
+
+.nav-list li {
+  list-style: none;
+}
+
+.nav-list a {
+  text-decoration: none; 
+  color: inherit; 
+}
+
+.headerRight {
+  display: flex;
+  align-items: center;
+}
 </style>
