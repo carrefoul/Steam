a58cbdce2f5a2f18ab0e6c9f6fb14d7c346cf4bb
alexfeerre <afernandez5235@esdmadrid.es>
2024-05-25 16:04:30 +0200

    final anim

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <main class="index-page">
-    <div class="title-container" ref="titleContainer">
+    <div class="title-container" :style="{ top: isMobile ? '50%' : '5%' }" ref="titleContainer">
       <h1 class="title">
         <span class="bold">Steam</span> Is The Ultimate Destination For Playing, Discussing, And Creating Games.
       </h1>
@@ -16,20 +16,30 @@
 <script>
 export default {
   name: 'IndexPage',
+  data() {
+    return {
+      isMobile: false // Variable para determinar si la pantalla es móvil
+    };
+  },
   mounted() {
     // Inicializa la opacidad del título en 0 al cargar la página
     this.$refs.titleContainer.style.opacity = '0';
+    // Verifica si la pantalla es móvil
+    this.isMobile = window.innerWidth < 700;
   },
   methods: {
-    
     videoEnded() {
       // Muestra el título con animaciones de opacidad y movimiento hacia abajo
       const title = this.$refs.titleContainer;
-      title.style.transform = 'translateY(-50%)';
       title.style.transition = 'opacity 1.0s ease-in, transform 1.0s ease-out';
       title.style.opacity = '1';
-      title.style.transform = 'translateY(0)';
+      if (this.isMobile) {
+        title.style.transform = 'translateY(-200%)';
+      } else {
+        title.style.transform = 'translateY(0)';
+      }
     }
+   
   }
 };
 </script>
@@ -51,7 +61,6 @@ export default {
   transform: translateY(-150%);
   width: 300px; 
   word-wrap: break-word;
-  margin-top: 5%;
 }
 
 .title {
@@ -79,5 +88,13 @@ video {
   mix-blend-mode: screen;
   display: block;
   outline: none;
+}
+
+@media (max-width: 700px) {
+  .title-container {
+    left: 8%;
+    transform: translateY(-250%);
+  }
+  
 }
 </style>
