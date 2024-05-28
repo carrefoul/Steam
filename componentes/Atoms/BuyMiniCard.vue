db69b793ad367ae300e83a0eb5d7d623e42b6c7c
katttalin <kattalinbarcena@gmail.com>
2024-05-28 16:40:17 +0200

    buylogin

--- componentes/Atoms/BuyMiniCard.vue
+++ componentes/Atoms/BuyMiniCard.vue
@@ -1,13 +1,47 @@
 <template>
-  <div class="BuyMiniCard">
+  <div v-if="loggedIn" class="BuyMiniCard">
     <BuyLink :showSale="true" textSize="h2" :showText="true" buttonText="BUY BUTTON" />
     <div class="MiniCard">
       <IconLink class="adjust-space" textSize="p" :fondoAzul="true" :showText="true" buttonText="Añadir al carro" />
       <BuyLink :fontChange="true" :showBox="true" textSize="p" :showText="true" buttonText="Ver más" />
     </div>
   </div>
+  <div v-else class="BuyMiniCard">
+    <BuyLink :showSale="true" textSize="h2" :showText="true" buttonText="BUY BUTTON" />
+    <div class="MiniCard">
+      <BuyLink :fontChange="true" :showBox="true" textSize="p" :showText="true" buttonText="Ver más" />
+    </div>
+  </div>
 </template>
 
+<script>
+import IconLink from '@/componentes/Atoms/IconLink.vue';
+import BuyLink from '@/componentes/Atoms/BuyLink.vue';
+export default {
+  components: {
+  IconLink,
+  BuyLink
+  },
+  
+data() {
+  return {
+    loggedIn: false
+  };
+},
+mounted() {
+  this.checkLoginStatus();
+  window.addEventListener('storage', this.checkLoginStatus);
+},
+beforeDestroy() {
+  window.removeEventListener('storage', this.checkLoginStatus);
+},
+methods: {
+  checkLoginStatus() {
+    this.loggedIn = localStorage.getItem('isLoggedIn') === 'true';
+  }
+}
+};
+</script>
 
 <style lang="postcss">
 .MiniCard{
