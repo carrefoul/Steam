576f6f4c6e1fa136565df823e794e3c132d6d2c5
carrefoul <mrotaru5206@esdmadrid.es>
2024-05-29 19:09:25 +0200

    link a detalles juego

---
+++ pages/detalleJuego.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="detalle-juego">
+      <h1>Detalle del Juego</h1>
+      <!-- Agrega el contenido detallado del juego aquí -->
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'DetalleJuego'
+  };
+  </script>
+  
+  <style scoped>
+  .detalle-juego {
+    padding: 20px;
+  }
+  </style>
+  
