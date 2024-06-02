c1f93e6e737647629bf8a792c39a0807c866af5a
Patricia Garcia <159826462+ranita3000@users.noreply.github.com>
2024-06-02 17:26:11 +0200

    perfil

---
+++ componentes/Atoms/profileGame.vue
@@ -0,0 +1,98 @@
+<template>
+    <div class="container">
+        <div class="image-container">
+            <img :src="gameData.background_image" alt="Game Image" @load="adjustHeight" />
+        </div>
+        <div class="things">
+            <div class="left">
+                <div class="textCard">
+                    <h2>{{ gameData.name }}</h2>
+                </div>
+                <div class="barra">
+
+                </div>
+
+            </div>
+            <div class="right">
+                <BuyLink :showBox="true" textSize="h6"
+                    :showText="true" buttonText="Desinstalar" />
+            </div>
+        </div>
+
+    </div>
+
+
+
+
+
+</template>
+
+<script>
+
+export default {
+
+    props: {
+        gameData: {
+            type: Object,
+            required: true
+        }
+    },
+    
+};
+</script>
+
+
+
+<style scoped>
+.container{
+    display: flex;
+    flex-direction: row;
+    width: 100%;
+    gap: 20px;
+    height: 100px;
+}
+.image-container {
+    width: 180px;
+    height: 100px;
+    overflow: hidden;
+
+}
+
+.image-container img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+}
+.things{
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    width: 100%;
+    align-items: center;
+}
+.left{
+    display: flex;
+    flex-direction: column;
+    gap: 5px;
+}
+
+
+
+
+
+.textCard {
+    display: flex;
+    flex-direction: column;
+    flex: 1;
+    gap: 10px;
+
+    h3 {
+        font-family: AeonikTRIAL-Regular, Helvetica, sans-serif;
+        font-weight: normal;
+        font-size: 1.05rem;
+        margin: 0;
+    }
+}
+
+
+</style>
--- componentes/Moleculas/ProfileGames.vue
+++ componentes/Moleculas/ProfileGames.vue
@@ -1,31 +1,73 @@
 <template>
     <main class="all">
-        <div>
         <h8 class="profilename">JUEGOS</h8>
-        </div>
         <div class="friendstitle">
         <IconLink class="juegos" textSize="p" :pagina="true" :showText="true" buttonText="Todos los juegos" />
         <imputsElements class="ajusteinput" :showText="true" placeholderText="Buscar un juego" :showIcon="true" iconName="Lupa"
         textSize="h3" />
         </div>
+        <div class="containerBox">
+          <ProfileGame v-for="game in games" :key="game.id" :gameData="game" />
+          
+
+        </div>
+        
        
     </main>
 </template>
 
 <script>
+import axios from 'axios';
+
+
+
 
 import profilePicture from '~/componentes/Atoms/profilePicture.vue';
 import IconLink from '@/componentes/Atoms/IconLink.vue';
+import profileGame from '@/componentes/Atoms/profileGame.vue';
+
+const apiKey = 'c320afcffae4417e9b8004ba91f1950b';
 
 export default {
   components: {
     IconLink,
-    profilePicture
+    profilePicture,
+    profileGame
   },
+  data() {
+        return {
+            games: [],
+            
+        };
+    },
+    async mounted() {
+        await this.fetchGames();
+    },
   methods: {
     redirectToInsigniasPage() {
       this.$router.push('/insignias');
-    }
+    },
+    async fetchGames() {
+            const apiUrl = `https://api.rawg.io/api/games?key=${apiKey}&page_size=7`;
+
+            try {
+                const response = await axios.get(apiUrl);
+                const games = response.data.results;
+
+                const gameDetailsPromises = games.map(async (game) => {
+                    const gameDetailsUrl = `https://api.rawg.io/api/games/${game.id}?key=${apiKey}`;
+                    const gameDetailsResponse = await axios.get(gameDetailsUrl);
+                    return {
+                        ...game,
+                        description: gameDetailsResponse.data.description_raw || 'No description available'
+                    };
+                });
+
+                this.games = await Promise.all(gameDetailsPromises);
+            } catch (error) {
+                console.error('Error fetching game data:', error);
+            }
+        }
   }
 }
 </script>
@@ -34,7 +76,8 @@ export default {
 .all{
     display: flex;
     flex-direction: column;
-    padding: 1.563rem;
+    padding: 20px;
+    gap: 10px;
 }
 .profilename{
     display: flex;
@@ -64,6 +107,11 @@ justify-content: center;
 .friendsbox{
     display: flex;
     flex-direction: row;
+}
+.containerBox{
+  display: flex;
+  flex-direction: column;
+  gap: 5px;
 }
 
 </style>
